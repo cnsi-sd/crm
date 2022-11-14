e70c81ef14dafe7ecc3d71049ab1f4dcd201efd1
Antoine Turmel <antoine.turmel@cnsi-sd.fr>
2022-11-14 17:08:50 +0100

    on vérifie si l'utilisateur est actif avant de se connecter

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -45,7 +45,10 @@ class LoginRequest extends FormRequest
     {
         $this->ensureIsNotRateLimited();
 
-        if (!Auth::attempt($this->only('email', 'password'), $this->filled('remember'))) {
+        $credentials = $this->only('email', 'password');
+        $credentials['active'] = true;
+
+        if (!Auth::attempt($credentials, $this->filled('remember'))) {
             RateLimiter::hit($this->throttleKey());
 
             throw ValidationException::withMessages([
