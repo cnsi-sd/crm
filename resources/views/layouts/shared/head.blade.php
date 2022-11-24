384e88f22cfb04a3d07e4fea37e0e52e5ac6bf01
Antoine Turmel <antoine.turmel@cnsi-sd.fr>
2022-11-24 17:54:56 +0100

    enlever le thème dark, ajout du logout

--- resources/views/layouts/shared/head.blade.php
+++ resources/views/layouts/shared/head.blade.php
@@ -12,4 +12,3 @@
 <!-- App css -->
 <link href="{{asset('assets/css/icons.min.css')}}" rel="stylesheet" type="text/css" />
 <link href="{{asset('assets/css/app.min.css')}}" rel="stylesheet" type="text/css" id="light-style" />
-<link href="{{asset('assets/css/app-dark.min.css')}}" rel="stylesheet" type="text/css" id="dark-style" />
--- resources/views/layouts/shared/horizontal-nav.blade.php
+++ resources/views/layouts/shared/horizontal-nav.blade.php
@@ -211,7 +211,7 @@
                     </a>
 
                     <!-- item-->
-                    <a href="javascript:void(0);" class="dropdown-item notify-item">
+                    <a href="{{route('second', ['auth', 'logout'])}}" class="dropdown-item notify-item">
                         <i class="mdi mdi-logout me-1"></i>
                         <span>Logout</span>
                     </a>
