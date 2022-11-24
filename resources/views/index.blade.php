50797cd7cae76d9547866bb5cadad09d5f9b7043
Antoine Turmel <antoine.turmel@cnsi-sd.fr>
2022-11-24 18:13:03 +0100

    virer les notifications/réseaux sociaux et les filtres sur la page d'accueil

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -15,22 +15,6 @@
     <div class="row">
         <div class="col-12">
             <div class="page-title-box">
-                <div class="page-title-right">
-                    <form class="d-flex">
-                        <div class="input-group">
-                            <input type="text" class="form-control form-control-light" id="dash-daterange">
-                            <span class="input-group-text bg-primary border-primary text-white">
-                                <i class="mdi mdi-calendar-range font-13"></i>
-                            </span>
-                        </div>
-                        <a href="javascript: void(0);" class="btn btn-primary ms-2">
-                            <i class="mdi mdi-autorenew"></i>
-                        </a>
-                        <a href="javascript: void(0);" class="btn btn-primary ms-1">
-                            <i class="mdi mdi-filter-variant"></i>
-                        </a>
-                    </form>
-                </div>
                 <h4 class="page-title">Dashboard</h4>
             </div>
         </div>
