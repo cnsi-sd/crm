fbcaa3b73cf1862f7aae4e6e3751cf4e604637ef
Antoine Turmel <antoine.turmel@cnsi-sd.fr>
2022-11-29 12:53:15 +0100

    assets js dans le bon ordre

--- resources/views/layouts/shared/footer-script.blade.php
+++ resources/views/layouts/shared/footer-script.blade.php
@@ -1,8 +1,7 @@
 <!-- bundle -->
 <script src="{{asset('assets/js/vendor.js')}}"></script>
 @yield('script')
-<script src="{{asset('assets/js/app.min.js')}}"></script>
-{{-- <script src="{{ Vite::asset('resources/js/app.js') }}"></script>
+<script src="{{ Vite::asset('resources/js/app.js') }}"></script>
+<script src="{{ Vite::asset('resources/js/layout.js') }}"></script>
 <script src="{{ Vite::asset('resources/js/hyper.js') }}"></script>
-<script src="{{ Vite::asset('resources/js/layout.js') }}"></script> --}}
 @yield('script-bottom')
