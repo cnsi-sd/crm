afadbea3edae3222070657850cef390c4c8e8acb
Charly Caillon <charly.caillon@cnsi-sd.fr>
2022-12-07 09:12:03 +0100

    Fix favicon

--- resources/views/layouts/shared/head.blade.php
+++ resources/views/layouts/shared/head.blade.php
@@ -3,7 +3,8 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 <!-- App favicon -->
-<link rel="shortcut icon" href="{{asset('/assets/images/favicon.ico')}}">
+<link rel="icon" href="{{asset('/favicon.ico')}}" />
+<link rel="shortcut icon" href="{{asset('/favicon.ico')}}">
 
 @yield('css')
 
