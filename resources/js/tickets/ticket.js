439199d8749743be744ddc9c6df797a6cdf2d79a
Antoine Turmel <antoine.turmel@cnsi-sd.fr>
2023-01-18 10:48:12 +0100

    fix jquery

--- resources/js/tickets/ticket.js
+++ resources/js/tickets/ticket.js
@@ -1,7 +1,6 @@
 $( document ).ready(function() {
     $('.card-header').click(function () {
-        console.log($(this).data("comment-id"));
-        $.get('/tickets/hide_comment/' + $(this).data("comment-id"), function(data) {
+            $.get('/tickets/hide_comment/' + $(this).data("comment-id"), function(data) {
         });
     });
 });
