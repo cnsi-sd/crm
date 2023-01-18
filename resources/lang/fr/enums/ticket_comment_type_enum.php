168af62de8657e760367b9f17ab6dbebefe8ea05
Antoine Turmel <antoine.turmel@cnsi-sd.fr>
2023-01-18 10:42:12 +0100

    responsible_info passe à manager_info

--- resources/lang/fr/enums/ticket_comment_type_enum.php
+++ resources/lang/fr/enums/ticket_comment_type_enum.php
@@ -3,7 +3,7 @@
 return array(
     'OTHERS' => 'Autre',
     'EXTERNAL_CONTACT' => 'Contact externe',
-    'RESPONSIBLE_INFO' => 'Information du responsable',
+    'MANAGER_INFO' => 'Information du responsable',
     'PROCESS_TO_FOLLOW' => 'Process à suivre',
     'INFO_IMPORTANT' => 'Info importante',
     'SUMMARY' => 'Résumé',
