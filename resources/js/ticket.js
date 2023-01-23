55b4b49a54056232762aa7e6aafb8f591358df82
Mathias <mathias.ouanna@cnsi-sd.fr>
2023-01-23 16:04:22 +0100

    view before changing fore auto reload page

--- resources/js/ticket.js
+++ resources/js/ticket.js
@@ -1,43 +1,74 @@
 let inputLine = document.getElementById('number-list');
 let bodyCard = document.getElementById('card-body-tag');
 
-document.getElementById('add').onclick = updateValue;
+document.getElementById('add').onclick = addListTagOnThread;
+$('.tags').on('select2:select', saveTicketThreadTags);
+$('.deleteTaglist').on("click", deleteTagLists)
 
-function updateValue(){
+function addListTagOnThread(e){
+    // create new line in db
+    let lineId;
+    let thread_id = e.target.getAttribute("data-thread_id")
+    window.axios.post('/addTagList', {
+        thread_id: thread_id
+    }).then(function (response) {
+        /*console.log(response);
+        lineId = response;
 
-    inputLine.value = Number(inputLine.value) + 1;
-    let divLine = document.createElement("div");
-    divLine.id = "line-" + inputLine.value;
-    bodyCard.appendChild(divLine);
+        //create div for create line
+        let divLine = document.createElement("div");
+        divLine.id = "line-" + lineId;
+        bodyCard.appendChild(divLine);
 
-    let selectTag = document.createElement("select");
-    selectTag.name = "thread-tags" + inputLine.value;
-    selectTag.className = "form-select";
-    makeOption(selectTag);
-    divLine.appendChild(selectTag);
+        //create select tag
+        let selectTag = document.createElement("select");
+        selectTag.name = "thread-tags-" + lineId;
+        selectTag.className = "form-select";
+        selectTag.setAttribute("data-thread_id", thread_id);
+        selectTag.setAttribute("data-taglist_id", lineId)
+        makeOption(selectTag);
+        divLine.appendChild(selectTag);
 
-    $(selectTag).select2()
-    $(selectTag).on('select2:select', saveTicketTags)
+        //add select2 on selectTag
+        $(selectTag).select2()
+        $(selectTag).on('select2:select', saveTicketTags)*/
+        location.reload()
+    })
 }
 
 function makeOption(select){
     window.axios.get('/ajaxTags').then(function (response){
-        response.json().then(function (json) {
+        let json = response.data.data;
+        let option = document.createElement('option')
+        option.text = "Aucune";
+        select.add(option)
+        json.forEach(function (data) {
             let option = document.createElement('option')
-            option.text = "Aucune";
+            option.text = data.name;
+            option.value = data.id;
             select.add(option)
-            json.data.forEach(function (data) {
-                let option = document.createElement('option')
-                option.text = data.name;
-                option.value = data.id;
-                select.add(option)
-            })
-
-
         })
     })
 }
 
-function saveTicketTags(e) {
-    console.log(e)
+function saveTicketThreadTags(e) {
+    let tag_id = e.target.options[e.target.options.selectedIndex].value;
+    let taglist_id = e.target.getAttribute("data-taglist_id");
+    window.axios.post('/saveTicketThreadTags', {
+        taglist_id: taglist_id,
+        tag_id: tag_id
+    }).then(
+        location.reload()
+    )
+}
+
+function deleteTagLists(e){
+    let thread_id = e.target.getAttribute("data-thread_id");
+    let taglist_id = e.target.getAttribute("data-taglist_id");
+    window.axios.post('/deleteTagList', {
+        thread_id: thread_id,
+        taglist_id: taglist_id
+    }).then(
+        location.reload()
+    )
 }
