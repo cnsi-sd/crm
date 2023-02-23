8064866b5492da276c28afa9b1bd521dd3d2c365
Antoine Turmel <antoine.turmel@cnsi-sd.fr>
2023-02-23 18:49:52 +0100

    helper floatToString en js, commentaire privé en html, date au format français

--- resources/js/tickets/ticket.js
+++ resources/js/tickets/ticket.js
@@ -6,18 +6,19 @@ $( document ).ready(function() {
 
     $('#order-info-tab').one("click", function () {
         $.get('/tickets/get_external_infos/' + $(this).data("order-id"), function(data) {
-            console.log(data);
             $("#ext-order-link").attr('href', $("#ext-order-link").attr('href') + data.externalOrderInfo.reference);
             $("#ext-order-id").text(data.externalOrderInfo.id_order);
             $("#ext-order-state").text(data.externalOrderInfo.state);
-            $("#ext-order-date").text(data.externalOrderInfo.date_add); //d/m/Y H:i
+            var order_date = new Date(data.externalOrderInfo.date_add);
+            $("#ext-order-date").text(order_date.toLocaleDateString("fr") + ' ' + order_date.toLocaleTimeString("fr")); //d/m/Y H:i
             $("#ext-order-carrier").text(data.externalOrderInfo.carrier);
-            $("#ext-order-tracking").attr('href', data.externalAdditionalOrderInfo.tracking_info[0]['url'].replace('@', data.externalAdditionalOrderInfo.tracking_info[0]['tracking_number']));
+            $("#ext-order-tracking").attr('href', data.externalAdditionalOrderInfo.tracking_info[0]['url']
+                .replace('@', data.externalAdditionalOrderInfo.tracking_info[0]['tracking_number']));
             $("#ext-order-tracking").text(data.externalAdditionalOrderInfo.tracking_info[0]['tracking_number']);
-            $("#ext-order-total-paid").text(data.externalOrderInfo.total_paid); // €
-            $("#ext-order-margin").text(data.externalAdditionalOrderInfo.margin); // €
+            $("#ext-order-total-paid").text(floatToString(data.externalOrderInfo.total_paid, "€", 2));
+            $("#ext-order-margin").text(floatToString(data.externalAdditionalOrderInfo.margin, "€", 2));
             $("#ext-order-customer-email").text(data.externalOrderInfo.customer.email);
-            $("#ext-order-note").text(data.externalOrderInfo.note);
+            $("#ext-order-note").append(data.externalOrderInfo.note);
             $("#ext-order-billing-name").text(data.externalOrderInfo.billing_address.firstname + " " + data.externalOrderInfo.billing_address.lastname);
             $("#ext-order-billing-address1").text(data.externalOrderInfo.billing_address.address1);
             $("#ext-order-billing-address2").text(data.externalOrderInfo.billing_address.address2);
@@ -31,8 +32,20 @@ $( document ).ready(function() {
             $("#ext-order-shipping-phone").text(data.externalOrderInfo.shipping_address.phone);
             $("#ext-order-shipping-phone-mobile").text(data.externalOrderInfo.shipping_address.phone_mobile);
             data.externalOrderInfo.items.forEach(function(item){
-                $('#ext-order-items tbody').append('<tr><td>'+item.product_name+' - '+item.product_reference+' - '+item.product_ean13+'</td><td>'+item.product_quantity+'</td><td>'+data.externalSuppliers.find(element => element.id_supplier == item.id_definitive_supplier).name+'</td></tr>');
+                $('#ext-order-items tbody').append('<tr><td>'+item.product_name+' - '+item.product_reference+' - '+
+                    item.product_ean13+'</td><td>'+item.product_quantity+'</td><td>'+
+                    data.externalSuppliers.find(element => element.id_supplier == item.id_definitive_supplier).name+
+                    '</td></tr>');
             });
         });
-    })
+    });
+    function floatToString(value, currency, round) {
+        if(!value)
+            return '--';
+        if(round) {
+            round = Math.pow(10, round);
+            value = Math.round(value * round) / round;
+        }
+        return value.toString().replace('.',',') + ' ' + currency;
+    }
 });
