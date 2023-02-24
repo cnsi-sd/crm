9d3aed2b71a85ffede7c8ffbbdeb49e6fae6e6e4
Charly Caillon <charly.caillon@cnsi-sd.fr>
2023-02-24 12:16:22 +0100

    Add todo

--- database/seeders/prod/ChannelSeeder.php
+++ database/seeders/prod/ChannelSeeder.php
@@ -9,6 +9,7 @@ use Illuminate\Database\Seeder;
 class ChannelSeeder extends Seeder
 {
     public function run(){
+        // TODO : user 1 may not exists on the first seeder execution in production
         foreach (ChannelEnum::getList() as $value)
             Channel::firstOrCreate(['name' => $value],['user_id' => 1]);
     }
