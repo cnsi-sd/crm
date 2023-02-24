9ebc4734512c37a052830e3191e3389f8588cf0e
Laurent Le Foulgoc <laurent.lefoulgoc@cnsi-sd.fr>
2023-02-24 16:30:54 +0100

    The CheckActive middleware is now provided in \App\Http\Kernel::middlewareGroups

--- routes/web.php
+++ routes/web.php
@@ -30,75 +30,67 @@ use Illuminate\Support\Facades\Route;
 */
 
 require __DIR__ . '/auth.php';
-Route::middleware('checkActive')->group(function() {
+Route::get('/home', function () {
+    return redirect('/index');
+})->name('home');
 
-
-    Route::get('/home', function () {
-        return redirect('/index');
-    })->name('home');
-
-
-
-    Route::prefix('tickets')->group(function() {
-       Route::match(['get', 'post'], 'hide_comment/{comment}', [TicketController::class, 'hide_comment'])->name('hide_comment')->can('read', Ticket::class);
-       Route::match(['get', 'post'], 'all_tickets', [TicketController::class, 'all_tickets'])->name('all_tickets')->can('read', Ticket::class);
-       Route::match(['get', 'post'], 'user/{user}', [TicketController::class, 'user_tickets'])->name('user_tickets')->can('read', Ticket::class);
-       Route::match(['get', 'post'], '{ticket}', [TicketController::class, 'redirectTicket'])->name('ticket')->can('read', Ticket::class);
-       Route::match(['get', 'post'], '{ticket}/{thread}', [TicketController::class, 'ticket'])->name('ticket_thread')->can('read', Ticket::class);
-    });
-    Route::prefix('admin')->group(function () {
-            Route::prefix('roles')->group(function () {
-                Route::match(['get', 'post'], 'new', [RoleController::class, 'edit'])->name('create_role')->can('edit', Role::class);
-                Route::match(['get', 'post'], '{role}', [RoleController::class, 'edit'])->name('edit_role')->can('read', Role::class);
-                Route::match(['get', 'post'], '', [RoleController::class, 'list'])->name('roles')->can('read', Role::class);
-            });
-            Route::prefix('users')->group(function () {
-                Route::match(['get', 'post'], 'new', [UserController::class, 'edit'])->name('create_user')->can('edit', User::class);
-                Route::match(['get', 'post'], '{user}', [UserController::class, 'edit'])->name('edit_user')->can('edit', User::class);
-                Route::match(['get', 'post'], '', [UserController::class, 'list'])->name('users')->can('read', User::class);
-            });
-    });
-    Route::match(['get','post'], 'search', [SearchController::class, 'search'])->name('search')->can('read', Ticket::class);
-
-    Route::prefix('configuration')->group(function () {
-        Route::prefix('default_answer')->group(function () {
-            Route::match(['get', 'post'], 'new', [DefaultAnswerController::class, 'edit'])->name('create_defaultAnswer')->can('edit', DefaultAnswer::class);
-            Route::match(['get', 'post'], '{defaultAnswer}', [DefaultAnswerController::class, 'edit'])->name('edit_defaultAnswer')->can('edit', DefaultAnswer::class);
-            Route::match(['get', 'post'], '', [DefaultAnswerController::class, 'list'])->name('defaultAnswers')->can('read', DefaultAnswer::class);
-            Route::match(['get', 'post'], '{defaultAnswer}/delete', [DefaultAnswerController::class, 'delete'])->name('delete_defaultAnswers')->can('edit', DefaultAnswer::class);
+Route::prefix('tickets')->group(function() {
+   Route::match(['get', 'post'], 'hide_comment/{comment}', [TicketController::class, 'hide_comment'])->name('hide_comment')->can('read', Ticket::class);
+   Route::match(['get', 'post'], 'all_tickets', [TicketController::class, 'all_tickets'])->name('all_tickets')->can('read', Ticket::class);
+   Route::match(['get', 'post'], 'user/{user}', [TicketController::class, 'user_tickets'])->name('user_tickets')->can('read', Ticket::class);
+   Route::match(['get', 'post'], '{ticket}', [TicketController::class, 'redirectTicket'])->name('ticket')->can('read', Ticket::class);
+   Route::match(['get', 'post'], '{ticket}/{thread}', [TicketController::class, 'ticket'])->name('ticket_thread')->can('read', Ticket::class);
+});
+Route::prefix('admin')->group(function () {
+        Route::prefix('roles')->group(function () {
+            Route::match(['get', 'post'], 'new', [RoleController::class, 'edit'])->name('create_role')->can('edit', Role::class);
+            Route::match(['get', 'post'], '{role}', [RoleController::class, 'edit'])->name('edit_role')->can('read', Role::class);
+            Route::match(['get', 'post'], '', [RoleController::class, 'list'])->name('roles')->can('read', Role::class);
         });
-        Route::prefix('revival')->group(function () {
-            Route::match(['get', 'post'], 'new', [RevivalController::class, 'edit'])->name('create_revival')->can('edit', Revival::class);
-            Route::match(['get', 'post'], '{revival}', [RevivalController::class, 'edit'])->name('edit_revival')->can('edit', Revival::class);
-            Route::match(['get', 'post'], '', [RevivalController::class, 'list'])->name('revival')->can('read', Revival::class);
-            Route::match(['get', 'post'], '{revival}/delete', [RevivalController::class, 'delete'])->name('delete_revival')->can('edit', Revival::class);
+        Route::prefix('users')->group(function () {
+            Route::match(['get', 'post'], 'new', [UserController::class, 'edit'])->name('create_user')->can('edit', User::class);
+            Route::match(['get', 'post'], '{user}', [UserController::class, 'edit'])->name('edit_user')->can('edit', User::class);
+            Route::match(['get', 'post'], '', [UserController::class, 'list'])->name('users')->can('read', User::class);
         });
-        Route::prefix('autoReply')->group(function () {
-            Route::match(['get', 'post'], '', [AutoReplyController::class, 'edit'])->name('autoReply');
-        });
-        Route::prefix('tags')->group(function () {
-            Route::match(['get', 'post'], 'new', [TagsController::class, 'edit'])->name('create_tags')->can('edit', Tag::class);
-            Route::match(['get', 'post'], '{tags}', [TagsController::class, 'edit'])->name('edit_tags')->can('edit', Tag::class);
-            Route::match(['get', 'post'], '', [TagsController::class, 'list'])->name('tags')->can('read', Tag::class);
-            Route::match(['get', 'post'], '{tags}/delete', [TagsController::class, 'delete'])->name('delete_tags')->can('edit', Tag::class);
-        });
+});
+Route::match(['get','post'], 'search', [SearchController::class, 'search'])->name('search')->can('read', Ticket::class);
+
+Route::prefix('configuration')->group(function () {
+    Route::prefix('default_answer')->group(function () {
+        Route::match(['get', 'post'], 'new', [DefaultAnswerController::class, 'edit'])->name('create_defaultAnswer')->can('edit', DefaultAnswer::class);
+        Route::match(['get', 'post'], '{defaultAnswer}', [DefaultAnswerController::class, 'edit'])->name('edit_defaultAnswer')->can('edit', DefaultAnswer::class);
+        Route::match(['get', 'post'], '', [DefaultAnswerController::class, 'list'])->name('defaultAnswers')->can('read', DefaultAnswer::class);
+        Route::match(['get', 'post'], '{defaultAnswer}/delete', [DefaultAnswerController::class, 'delete'])->name('delete_defaultAnswers')->can('edit', DefaultAnswer::class);
     });
-
-    // CALL AJAX
-    Route::get('/ajaxTags', [TagsController::class, 'ajax_tags']);
-    Route::post('/addTagList', [TagsController::class, 'newTagLigne']);
-    Route::post('/saveTicketThreadTags', [TicketController::class, 'saveThreadTags']);
-    Route::post('/deleteTagList', [TicketController::class, 'delete_ThreadTagList']);
-    Route::post('/deleteThreadTagOnTagList', [TicketController::class, 'delete_tag']);
-
-    Route::group(['prefix' => '/'], function () {
-        Route::get('', function () {
-            return redirect('/login');
-        })->name('root');
-        Route::get('{first}/{second}/{third}', [RoutingController::class, 'thirdLevel'])->name('third');
-        Route::get('{first}/{second}', [RoutingController::class, 'secondLevel'])->name('second');
-        Route::get('{any}', [RoutingController::class, 'root'])->name('any');
+    Route::prefix('revival')->group(function () {
+        Route::match(['get', 'post'], 'new', [RevivalController::class, 'edit'])->name('create_revival')->can('edit', Revival::class);
+        Route::match(['get', 'post'], '{revival}', [RevivalController::class, 'edit'])->name('edit_revival')->can('edit', Revival::class);
+        Route::match(['get', 'post'], '', [RevivalController::class, 'list'])->name('revival')->can('read', Revival::class);
+        Route::match(['get', 'post'], '{revival}/delete', [RevivalController::class, 'delete'])->name('delete_revival')->can('edit', Revival::class);
     });
+    Route::prefix('autoReply')->group(function () {
+        Route::match(['get', 'post'], '', [AutoReplyController::class, 'edit'])->name('autoReply');
+    });
+    Route::prefix('tags')->group(function () {
+        Route::match(['get', 'post'], 'new', [TagsController::class, 'edit'])->name('create_tags')->can('edit', Tag::class);
+        Route::match(['get', 'post'], '{tags}', [TagsController::class, 'edit'])->name('edit_tags')->can('edit', Tag::class);
+        Route::match(['get', 'post'], '', [TagsController::class, 'list'])->name('tags')->can('read', Tag::class);
+        Route::match(['get', 'post'], '{tags}/delete', [TagsController::class, 'delete'])->name('delete_tags')->can('edit', Tag::class);
+    });
+});
 
+// CALL AJAX
+Route::get('/ajaxTags', [TagsController::class, 'ajax_tags']);
+Route::post('/addTagList', [TagsController::class, 'newTagLigne']);
+Route::post('/saveTicketThreadTags', [TicketController::class, 'saveThreadTags']);
+Route::post('/deleteTagList', [TicketController::class, 'delete_ThreadTagList']);
+Route::post('/deleteThreadTagOnTagList', [TicketController::class, 'delete_tag']);
 
+Route::group(['prefix' => '/'], function () {
+    Route::get('', function () {
+        return redirect('/login');
+    })->name('root');
+    Route::get('{first}/{second}/{third}', [RoutingController::class, 'thirdLevel'])->name('third');
+    Route::get('{first}/{second}', [RoutingController::class, 'secondLevel'])->name('second');
+    Route::get('{any}', [RoutingController::class, 'root'])->name('any');
 });
