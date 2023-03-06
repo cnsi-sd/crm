275d600b8c329b7d3c105d352d4a85b2dd0459ee
Mathias <mathias.ouanna@cnsi-sd.fr>
2023-03-06 10:06:48 +0100

    export tag on parts file

---
+++ resources/views/tickets/parts/tags.blade.php
@@ -0,0 +1,43 @@
+<div class="card">
+    <div class="card-header d-flex">
+        <p class="w-100">{{ trans_choice('app.tags.tags', 2) }}</p>
+        <button type="button" id="add" class="btn btn-success flex-shrink-1"
+                data-thread_id="{{$thread->id}}" data-url_add_tag="{{route('addTagList')}}">+
+        </button>
+    </div>
+    <div class="card-body" id="card-body-tag">
+        @foreach($thread->tagList as $taglist)
+            <div id="list-{{$taglist->id}}">
+                <button type="button" id="deleteTaglist-{{$taglist->id}}"
+                        class="deleteTaglist btn btn-danger"
+                        data-thread_id="{{ $thread->id }}"
+                        data-taglist_id="{{$taglist->id }}"
+                >x
+                </button>
+                <select name="ticket-revival" class="form-select no-sort tags"
+                        data-thread_id="{{$thread->id}}"
+                        data-taglist_id="{{$taglist->id}}">
+                    <option value="">{{ __('app.revival.select_revival') }}</option>
+                    @foreach (\App\Models\Tags\Tag::all() as $optionTag)
+                        <option value="{{ $optionTag->id }}">
+                            {{ $optionTag->name }}
+                        </option>
+                    @endforeach
+                </select>
+                <div id="view-{{$taglist->id}}" class="mt-3 mb-2">
+                    @foreach($taglist->tags as $tag)
+                        <span class="tags-style"
+                              style="background-color: {{ $tag->background_color }}; color: {{ $tag->text_color }};">
+                                                    {{ $tag->name }} | <button
+                                class="btn delete-tag"
+                                data-tag_id="{{$tag->id}}"
+                                data-taglist_id="{{$taglist->id}}"
+                                style="color: {{ $tag->text_color }};"> x </button>
+                                                </span>
+                    @endforeach
+                </div>
+                <hr/>
+            </div>
+        @endforeach
+    </div>
+</div>
