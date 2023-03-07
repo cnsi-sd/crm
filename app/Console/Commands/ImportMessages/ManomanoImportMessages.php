34e7c14f01be1a5cf757bcd35b13e34d34bc5d67
Laurent Le Foulgoc <laurent.lefoulgoc@cnsi-sd.fr>
2023-03-07 13:58:32 +0100

    processing of messages according to their subjects or their sender - wip

--- app/Console/Commands/ImportMessages/ManomanoImportMessages.php
+++ app/Console/Commands/ImportMessages/ManomanoImportMessages.php
@@ -3,7 +3,9 @@
 namespace App\Console\Commands\ImportMessages;
 
 use App\Enums\Channel\ChannelEnum;
+use App\Enums\Ticket\TicketMessageAuthorTypeEnum;
 use App\Models\Channel\Channel;
+use App\Models\Channel\Order;
 use App\Models\Ticket\Thread;
 use App\Models\Ticket\Ticket;
 use Cnsi\Logger\Logger;
@@ -15,7 +17,7 @@ class ManomanoImportMessages extends AbstractImportMessages
 {
     /** @var Mailbox */
     private Mailbox $mailbox;
-    const FROM_DATE_TRANSFORMATOR = ' - 2 hours';
+    const FROM_DATE_TRANSFORMATOR = ' - 72 hours';
 
     public function __construct()
     {
@@ -52,17 +54,31 @@ class ManomanoImportMessages extends AbstractImportMessages
             $this->logger->info('--- Get Emails details');
             foreach($this->getEmails($emailIds) as $emailId => $email){
                 $sender = $email->senderAddress;
-                    if(strpos('ne-pas-repondre', $sender))
-                        return;
 
-                $body =($email->textHtml);
-                $subject = $email->subject;
-                $messageId = $email->messageId; // todo parse id <[idToParse]@swift.generated>
-                $parsedMessage = $this->parseMessage($email->textHtml);
+                // Check if sender is "ne-pas-repondre@manomano.fr"
+                $doNotReply = strpos($sender, 'repondre');
+                if($doNotReply)
+                    continue;
+
+                $support = strpos($sender,'support');
+                if($support)
+                    $authorType = TicketMessageAuthorTypeEnum::OPERATOR;
+                else
+                    $authorType = TicketMessageAuthorTypeEnum::CUSTOMER;
+
+                $message = $this->messageTreatment($email, $from_time);
+
+                $message['date']    = $from_time;
+                $message['subject'] = $email->subject;
+                $message['id']      = $email->messageId;
+
+                $order      = Order::getOrder($message['orderId'], $this->channel);
+                $ticket     = Ticket::getTicket($order, $this->channel);
+                $thread     = Thread::getOrCreateThread($ticket, $message['orderId'], $email->subject, '');
+
+
                 $test = '';
             }
-
-            $test = "";
         } catch (Exception $e){
             $this->logger->error('An error has occurred. Rolling back.', $e);
             \App\Mail\Exception::sendErrorMail($e, $this->getName(), $this->description, $this->output);
@@ -123,10 +139,59 @@ class ManomanoImportMessages extends AbstractImportMessages
         return $emails;
     }
 
-    private function parseMessage($htmlEmail): array
+    private function parseHtmlMessage($htmlEmail)
     {
         $pattern = "/Message:(.*)<br/";
         preg_match($pattern,$htmlEmail,$matches);
         return $matches;
     }
+
+    private function parseOrderId($subject)
+    {
+        // get the orderId (Mxxxxxxxxxxxx pattern) in subjet
+        preg_match('/M(\d{12})',$subject, $orderMatche);
+
+        if(isset($orderMatche[1])){
+            return $orderMatche[1];
+        }
+        return false;
+    }
+
+    public function messageTreatment($email, $from_time)
+    {
+        $message = [];
+        /**
+         * on vérif le sender; si contient "repondre" on tèj, s'il contient "support" à voir le traitement mais c'est pas clair ( autant le traiter comme les autres )
+         * on vérif le subject => on prend le numéro de commande M(\d+12) ( qui sera le numéro de thread et l'orderId; le default "manomano_support"  )
+         * Si c'est [
+         *  une demande de facture,
+         *  demande d'information
+         *  ]
+         *  => on remplie le sujet, thread number, order, mais pour pour le content, on écrit un message bateau.
+         *
+         * on vérif s'il existe le textPlain, sinon on prend le textHtml.
+         */
+
+        $isReply = $email->replyTo;
+        $messageDate = $from_time;
+        $body = $email->textHtml;
+        $plainBody = $email->textPlain;
+        $subject = $email->subject;
+        $attachment = $email->getAttachments();
+        $messageId = $email->messageId; // todo parse id <[idToParse]@swift.generated>
+        $parsedMessage = $this->parseHtmlMessage($email->textHtml);
+
+        if(strpos('Demande de facture',$subject))
+            $message['content'] = 'Pouvez-vous répondre à cet email avec la facture au format pdf en pièce-jointe svp?';
+
+        if(strlen($email->textPlain))
+            $message['content'] = $email->textPlain;
+
+        $message['orderId'] = $this->parseOrderId($subject);
+
+
+        return $message;
+    }
+
+
 }
