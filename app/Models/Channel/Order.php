39cea7b54ccaf04044b5c8ac299cf5f9b0751613
Charly Caillon <charly.caillon@cnsi-sd.fr>
2023-03-09 17:05:59 +0100

    Fix Prestashop Order cache

--- app/Models/Channel/Order.php
+++ app/Models/Channel/Order.php
@@ -29,7 +29,7 @@ class Order extends Model
         'updated_at'
     ];
 
-    protected ?array $_prestashopOrders;
+    protected ?array $_prestashopOrders = null;
 
     /**
      * @param string $orderId
