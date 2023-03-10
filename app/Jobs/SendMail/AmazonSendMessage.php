56256765701ecf1ee6738fc38996712d25f15ec3
Charly Caillon <charly.caillon@cnsi-sd.fr>
2023-03-10 15:33:19 +0100

    Refacto AmazonSendMessage using AbstractSendMessage

--- app/Jobs/SendMail/AmazonSendMessage.php
+++ app/Jobs/SendMail/AmazonSendMessage.php
@@ -2,32 +2,17 @@
 
 namespace App\Jobs\SendMail;
 
-use App\Models\Channel\Channel;
-use App\Models\Ticket\Message;
-use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Foundation\Bus\Dispatchable;
-use Illuminate\Queue\InteractsWithQueue;
-use Illuminate\Queue\SerializesModels;
+use App\Jobs\SendMessage\AbstractSendMessage;
 use Illuminate\Support\Facades\Mail;
 
-class AmazonSendMessage implements ShouldQueue
+class AmazonSendMessage extends AbstractSendMessage
 {
-    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
-
-    public Message $message;
-    protected Channel $channel;
-
-    public function __construct(Message $message)
+    protected function sendMessage(): void
     {
-        $this->message = $message;
-    }
-
-    public function handle(){
         $data["email"] = json_decode($this->message->thread->channel_data)['replyTo'];
         $data["title"] = "Re :" . $this->message->thread->name;
         $data["body"] = $this->message->content;
-        Mail::send('emails.ticketDelay', $data, function($message)use($data) {
+        Mail::send('emails.ticketDelay', $data, function ($message) use ($data) {
             $message->to($data["email"], $data["email"])
                 ->subject($data["title"]);
         });
