48d40e89c148e00c63a123eeafad911977396a90
Charly Caillon <charly.caillon@cnsi-sd.fr>
2023-03-10 16:15:25 +0100

    getUnreadMessages Improvement

--- resources/views/tickets/ticket.blade.php
+++ resources/views/tickets/ticket.blade.php
@@ -127,8 +127,8 @@
                             <span @class([
                                     'position-absolute top-0 start-100 translate-middle badge rounded-pill',
                                     'bg-danger' => $ticketThread->getUnreadMessages() > 0,
-                                    'bg-success' => $ticketThread->getUnreadMessages() === 0]
-                            )>
+                                    'bg-success' => $ticketThread->getUnreadMessages() === 0
+                            ])>
                                 {{ $ticketThread->getUnreadMessages() }}
                             </span>
                             <a class="nav-link @if($ticketThread->id === $thread['id']) active @endif" aria-current="page" href="{{ route('ticket_thread', ['ticket' => $ticket->id, 'thread' => $ticketThread->id]) }}">
