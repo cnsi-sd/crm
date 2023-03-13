ee6c45072c74803c592e0f31d18ca9f9e900ea7b
Charly Caillon <charly.caillon@cnsi-sd.fr>
2023-03-13 17:01:43 +0100

    SAV process accept multiple channel names

---
+++ app/Helpers/Prestashop/SavProcessGateway.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Helpers\Prestashop;
+
+use App\Models\Ticket\Ticket;
+
+final class SavProcessGateway
+{
+    public static function getUrl(Ticket $ticket): string
+    {
+        // Build endpoint
+        $endpoint = env('PRESTASHOP_URL') . 'procedure-sav';
+
+        // Build query parameters
+        $queryParams = [
+            'mp_order' => $ticket->order->channel_order_number,
+            'mp_names' => implode(',', $ticket->channel->ext_names),
+            'id_ticket' => $ticket->id,
+            'admintoken' => env('PRESTASHOP_CUSTOMER_SERVICE_TOKEN'),
+        ];
+
+        // Transform parameters array to query string
+        $queryString = http_build_query($queryParams);
+
+        // Build full URL
+        return sprintf('%s?%s', $endpoint, $queryString);
+    }
+}
