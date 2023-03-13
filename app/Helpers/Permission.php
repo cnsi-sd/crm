40a6e822a409cd586449f54e306a3c788b0c9e59
Charly Caillon <charly.caillon@cnsi-sd.fr>
2023-03-13 14:39:00 +0100

    Role form : move user et role permissions in the new admin tab

--- app/Helpers/Permission.php
+++ app/Helpers/Permission.php
@@ -10,30 +10,6 @@ abstract class Permission
     {
         return [
             [
-                'title' => trans_choice('app.permission.user', 2),
-                'sub_sections' => [
-                    [
-                        'title' => trans_choice('app.permission.user',2),
-                        'items' => [
-                            PermissionEnum::USER_READ,
-                            PermissionEnum::USER_EDIT,
-                        ],
-                    ],
-                ],
-            ],
-            [
-                'title' => trans_choice('app.permission.role', 2),
-                'sub_sections' => [
-                    [
-                        'title' => trans_choice('app.permission.role',2),
-                        'items' => [
-                            PermissionEnum::ROLE_READ,
-                            PermissionEnum::ROLE_EDIT,
-                        ],
-                    ],
-                ],
-            ],
-            [
                 'title' => trans_choice('app.permission.default_message_answer', 2),
                 'sub_sections' => [
                     [
@@ -95,6 +71,20 @@ abstract class Permission
             [
                 'title' => __('app.navbar.admin'),
                 'sub_sections' => [
+                    [
+                        'title' => trans_choice('app.permission.user',2),
+                        'items' => [
+                            PermissionEnum::USER_READ,
+                            PermissionEnum::USER_EDIT,
+                        ],
+                    ],
+                    [
+                        'title' => trans_choice('app.permission.role',2),
+                        'items' => [
+                            PermissionEnum::ROLE_READ,
+                            PermissionEnum::ROLE_EDIT,
+                        ],
+                    ],
                     [
                         'title' => trans_choice('app.configuration.channel', 2),
                         'items' => [
