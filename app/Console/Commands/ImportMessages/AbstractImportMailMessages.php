156d5b3dfe0e6d1d262af7b140f11f4497abb771
Laurent Le Foulgoc <laurent.lefoulgoc@cnsi-sd.fr>
2023-03-20 16:38:33 +0100

    Console/Commands/ImportMessages/AbstractImportMailMessages.php is now an abstract class

--- app/Console/Commands/ImportMessages/AbstractImportMailMessages.php
+++ app/Console/Commands/ImportMessages/AbstractImportMailMessages.php
@@ -5,23 +5,8 @@ namespace App\Console\Commands\ImportMessages;
 use App\Models\Ticket\Thread;
 use App\Models\Ticket\Ticket;
 
-class AbstractImportMailMessages extends AbstractImportMessages
+abstract class AbstractImportMailMessages extends AbstractImportMessages
 {
-    protected function getCredentials(): array
-    {
-        // TODO: Implement getCredentials() method.
-    }
-
-    protected function initApiClient()
-    {
-        // TODO: Implement initApiClient() method.
-    }
-
-    protected function convertApiResponseToMessage(Ticket $ticket, $message_api_api, Thread $thread)
-    {
-        // TODO: Implement convertApiResponseToMessage() method.
-    }
-
     protected function search($query = []): array
     {
         if(empty($query)) {
