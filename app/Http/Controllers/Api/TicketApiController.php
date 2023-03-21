1d71a47cf42427affaa19d55914e8a2909ddf1f8
Antoine Turmel <antoine.turmel@cnsi-sd.fr>
2023-03-21 10:10:25 +0100

    on enlève l'AbstractApiController, on logge ce qu'il se passe dans TicketApiController et try/catch

--- app/Http/Controllers/Api/TicketApiController.php
+++ app/Http/Controllers/Api/TicketApiController.php
@@ -5,25 +5,37 @@ namespace App\Http\Controllers\Api;
 use App\Enums\Ticket\TicketStateEnum;
 use App\Models\Tags\Tag;
 use App\Models\Ticket\Ticket;
+use Cnsi\Logger\Logger;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Exception;
 
-class TicketApiController extends AbstractApiController
+class TicketApiController
 {
+    protected Logger $logger;
     public function savProcessComplete(Request $request, string $token, Ticket $ticket) : JsonResponse
     {
-        if($token === env('SAVPROCESS_CRM_TOKEN')) {
-            $ticket->deadline = date('Y-m-d');
-            $ticket->state = TicketStateEnum::WAITING_ADMIN;
-            //TODO $tagId = setting('savprocess.processcomplete.tag_id');
-            $tagId = env('SAVPROCESS_CRM_PROCESSCOMPLETE_TAG_ID');
-            $tag = Tag::findOrFail($tagId);
-            $ticket->addTag($tag);
-            $this->stopTicketRevival($ticket);
-            $ticket->save();
-            return response()->json(['status' => 'success']);
-        } else {
-            return response()->json(['status' => 'error']);
+        try {
+            $this->logger = new Logger('api/in/ticket/savProcessComplete.log', true, true);
+            $this->logger->info('--- START : updating ticket ' . $ticket->id . ' after sav process completed by customer ---');
+            if($token === env('SAVPROCESS_CRM_TOKEN')) {
+                $ticket->deadline = date('Y-m-d');
+                $ticket->state = TicketStateEnum::WAITING_ADMIN;
+                //TODO $tagId = setting('savprocess.processcomplete.tag_id');
+                $tagId = env('SAVPROCESS_CRM_PROCESSCOMPLETE_TAG_ID');
+                $tag = Tag::findOrFail($tagId);
+                $ticket->addTag($tag);
+                $this->stopTicketRevival($ticket);
+                $ticket->save();
+                $this->logger->info('--- DONE ---');
+                return response()->json(['status' => 'success']);
+            } else {
+                $this->logger->info('--- TOKEN NOT DEFINED ---');
+                return response()->json(['status' => 'error', 'error' => 'Token not defined']);
+            }
+        } catch (Exception $e) {
+            $this->logger->error($e->getMessage(), $e);
+            return response()->json(['status' => 'error', 'error' => $e]);
         }
     }
 
