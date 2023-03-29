f4f7cf3d5341351e25e2726209b85f873f3310ae
Laurent Le Foulgoc <laurent.lefoulgoc@cnsi-sd.fr>
2023-03-29 17:00:14 +0200

    wip

--- resources/js/tickets/ticket.js
+++ resources/js/tickets/ticket.js
@@ -56,6 +56,16 @@ $(document).ready(function () {
         $('[data-order-id=' + $(this).data("order-id") + ']').show();
     });
 
+    let mybutton = document.getElementById("btn-back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+    window.onscroll = function () {
+        alert()
+        scrollFunction();
+    };
+    // When the user clicks on the button, scroll to the top of the document
+    mybutton.addEventListener("click", backToTop);
+
 })
 
 let bodyCard = document.getElementById('card-body-tag');
@@ -176,5 +186,24 @@ function deleteTicketTag(e) {
     }).then(
         e.target.parentNode.remove()
     )
+
+
+
+    function scrollFunction() {
+        if (
+            document.body.scrollTop > 20 ||
+            document.documentElement.scrollTop > 20
+        ) {
+            mybutton.style.display = "block";
+        } else {
+            mybutton.style.display = "none";
+        }
+    }
+
+
+    function backToTop() {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+    }
 }
 
