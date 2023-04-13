f6ad51de45e29ba2baf65005dec0844ced8baf2e
Antoine Turmel <antoine.turmel@cnsi-sd.fr>
2023-04-13 18:14:30 +0200

    envoi de pièces jointes pour les MP utilisant EmailSendMessage

--- app/Jobs/SendMessage/EmailSendMessage.php
+++ app/Jobs/SendMessage/EmailSendMessage.php
@@ -17,6 +17,8 @@ class EmailSendMessage extends AbstractSendMessage
 
         $to = str_replace('"', "", $this->message->thread->channel_data);
 
+        $this->attachments = $this->message->documents()->get();
+
         if ($this->message->thread->name === Thread::EMAIL) {
             $subject = "Nouveau message au sujet de votre commande";
             if ($shopName = MessageVariable::NOM_BOUTIQUE->getSettingValue()) {
@@ -27,6 +29,11 @@ class EmailSendMessage extends AbstractSendMessage
         }
 
         $mail = new RawMail($subject, $this->message->content);
+        if($this->attachments->count() > 0) {
+            foreach ($this->attachments as $attachment) {
+                $mail->attach($attachment->getFilePath());
+            }
+        }
         Mail::to($to)->send($mail);
     }
 }
