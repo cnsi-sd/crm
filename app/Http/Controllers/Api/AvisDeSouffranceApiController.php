bc6a92bd2f4702172b002652e4d072a8f6bdf9fa
Mathias <mathias.ouanna@cnsi-sd.fr>
2023-05-09 17:18:39 +0200

    start docs

--- app/Http/Controllers/Api/AvisDeSouffranceApiController.php
+++ app/Http/Controllers/Api/AvisDeSouffranceApiController.php
@@ -7,11 +7,13 @@ use App\Enums\Ticket\TicketAvisDeSouffranceTypeEnum;
 use App\Enums\Ticket\TicketCommentTypeEnum;
 use App\Models\Channel\Channel;
 use App\Models\Channel\Order;
+use App\Models\Tags\Tag;
 use App\Models\Ticket\Comment;
 use App\Models\Ticket\Thread;
 use App\Models\Ticket\Ticket;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class AvisDeSouffranceApiController extends AbstractApiController
 {
@@ -19,23 +21,37 @@ class AvisDeSouffranceApiController extends AbstractApiController
     public function add_avis_de_souffrance(Request $request): JsonResponse
     {
         try {
-            $request->validate([
+            if ($request->header('token') !== env('NOTICE_OF_SUFFERING_TOKEN')) {
+                $this->logger->info('--- TOKEN is not available ---');
+                return $this->message('TOKEN is not available', 401, ['status' => 'error']);
+            }
+            $validator = Validator::make($request->all(), [
                 'channel' => ['required', 'string'],
                 'num_command' => ['required', 'string'],
                 'message_souffrance' => ['required', 'string'],
             ]);
+
+            // return HTTP 400 if validation fails
+            if ($validator->fails()) {
+                $this->logger->info('Validation failed');
+                return $this->message($validator->messages(), 400, ['status' => 'error']);
+            }
+
+            //transaction
             $channel = (new Channel())->getByName($request->input('channel'));
             $order = Order::getOrder($request->input('num_command'), $channel);
             $ticket = Ticket::getTicket($order, $channel);
-            $thread = Thread::getOrCreateThread($ticket,
+            Thread::getOrCreateThread($ticket,
                 Thread::DEFAULT_CHANNEL_NUMBER,
                 TicketAvisDeSouffranceTypeEnum::AVIS_DE_SOUFFRANCE . " - " . $request->input('num_command'));
-            $comment = Comment::getOrCreate($ticket->id, $request->input('message_souffrance'), TicketCommentTypeEnum::INFO_IMPORTANT);
+            Comment::getOrCreate($ticket->id, 'Raison : ' . $request->input('message_souffrance'), TicketCommentTypeEnum::INFO_IMPORTANT);
 
-            /*$tagId = setting('ticket.notice_of_suffering');
+            $tagId = setting('ticket.notice_of_suffering');
             $tag = Tag::findOrFail($tagId);
-            $ticket->addTag($tag);*/
+            $ticket->addTag($tag);
+
             return $this->message("Notice of suffering has been added", 200, ['status' => 'success']);
+
         } catch (\Exception $e) {
             $this->logger->error($e->getMessage(), $e);
             return $this->message('Internal Server Error', 500, ['status' => 'error']);
---
+++ doc/user-doc/docs/fonctionnalités/avis-de-souffrance.md
@@ -0,0 +1,10 @@
+# Avis de souffrance
+
+Un avis de souffrance est une alerte qui est ajoutée à un ticket quand il y a des soucis sur la commande en question
+
+## Fonctionnement
+
+Quand une commande sur le BO rencontre un problème, elle passe à un statut dit "Avis de souffrance".
+Ce statut sera donc automatiquement rajouter à la commande dédiée.
+
+Ce processus automatique ajoutera au ticket un commentaire privés avec la raison de la mise en souffrance de la commande
