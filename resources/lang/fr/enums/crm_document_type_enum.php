f908686ba9264849f4f86e676ded7718b412ee48
Laurent Le Foulgoc <laurent.lefoulgoc@cnsi-sd.fr>
2023-05-26 17:01:07 +0200

    :feat: ajout des types de document pj

--- resources/lang/fr/enums/crm_document_type_enum.php
+++ resources/lang/fr/enums/crm_document_type_enum.php
@@ -1,10 +1,17 @@
 <?php
 
 return array(
-    'CUSTOMER_SERVICE_REPORT' => "Rapport intervention SAV",
-    'CLIENT_BANK_ACCOUNT_NUMBER' => "RIB client",
-    'CUSTOMER_SERVICE_STATION' => "Station SAV",
-    'PRODUCT_PHOTO' => "Photo produit",
-    'CUSTOMER_FILING' => "Dépôt plainte client",
-    'OTHER' => "Autre",
+    'CUSTOMER_SERVICE_REPORT'       => "Rapport intervention SAV",
+    'CLIENT_BANK_ACCOUNT_NUMBER'    => "RIB client",
+    'CUSTOMER_SERVICE_STATION'      => "Station SAV",
+    'PRODUCT_PHOTO'                 => "Photo produit",
+    'CUSTOMER_FILING'               => "Dépôt plainte client",
+    'OTHER'                         => "Autre",
+    'CUSTOMER_INVOICE'              => "Facture",
+    'MANUAL_USE'                    => "Manuel d'utilisation",
+    'PREPAID_RETURN_TICKET'         => "Etiquette de retour prépayée",
+    'SLIP_RETURN'                   => "avoir",
+    'TYPE_RETURN_CONDITIONS'        => "Conditions de retour",
+    'TYPE_PHOTO'                    => "Photo",
+    'TYPE_PROTEST_DOC'              => "Document de litige transport",
 );
--- resources/views/tickets/parts/messages_document_types.blade.php
+++ resources/views/tickets/parts/messages_document_types.blade.php
@@ -6,6 +6,47 @@
         <option value="{{ \App\Enums\MessageDocumentTypeEnum::SLIP_RETURN }}">
             {{ \App\Enums\MessageDocumentTypeEnum::getMessage('SLIP_RETURN') }}
         </option>
+        <option value="{{ \App\Enums\MessageDocumentTypeEnum::CUSTOMER_RETURN }}">
+            {{ \App\Enums\MessageDocumentTypeEnum::getMessage('CUSTOMER_RETURN') }}
+        </option>
+        <option value="{{ \App\Enums\MessageDocumentTypeEnum::MANUAL_USE }}">
+            {{ \App\Enums\MessageDocumentTypeEnum::getMessage('MANUAL_USE') }}
+        </option>
+        <option value="{{ \App\Enums\MessageDocumentTypeEnum::TYPE_RETURN_CONDITIONS }}">
+            {{ \App\Enums\MessageDocumentTypeEnum::getMessage('TYPE_RETURN_CONDITIONS') }}
+        </option>
+        <option value="{{ \App\Enums\MessageDocumentTypeEnum::TYPE_PHOTO }}">
+            {{ \App\Enums\MessageDocumentTypeEnum::getMessage('TYPE_PHOTO') }}
+        </option>
+        <option value="{{ \App\Enums\MessageDocumentTypeEnum::TYPE_PROTEST_DOC }}">
+            {{ \App\Enums\MessageDocumentTypeEnum::getMessage('TYPE_PROTEST_DOC') }}
+        </option>
+        <option value="{{ \App\Enums\MessageDocumentTypeEnum::OTHER }}">
+            {{ \App\Enums\MessageDocumentTypeEnum::getMessage('OTHER') }}
+        </option>
+        @break
+    @case("fnac.com")
+        <option value="{{ \App\Enums\MessageDocumentTypeEnum::CUSTOMER_INVOICE }}">
+            {{ \App\Enums\MessageDocumentTypeEnum::getMessage('CUSTOMER_INVOICE') }}
+        </option>
+        <option value="{{ \App\Enums\MessageDocumentTypeEnum::SLIP_RETURN }}">
+            {{ \App\Enums\MessageDocumentTypeEnum::getMessage('SLIP_RETURN') }}
+        </option>
+        <option value="{{ \App\Enums\MessageDocumentTypeEnum::CUSTOMER_RETURN }}">
+            {{ \App\Enums\MessageDocumentTypeEnum::getMessage('CUSTOMER_RETURN') }}
+        </option>
+        <option value="{{ \App\Enums\MessageDocumentTypeEnum::MANUAL_USE }}">
+            {{ \App\Enums\MessageDocumentTypeEnum::getMessage('MANUAL_USE') }}
+        </option>
+        <option value="{{ \App\Enums\MessageDocumentTypeEnum::TYPE_RETURN_CONDITIONS }}">
+            {{ \App\Enums\MessageDocumentTypeEnum::getMessage('TYPE_RETURN_CONDITIONS') }}
+        </option>
+        <option value="{{ \App\Enums\MessageDocumentTypeEnum::TYPE_PHOTO }}">
+            {{ \App\Enums\MessageDocumentTypeEnum::getMessage('TYPE_PHOTO') }}
+        </option>
+        <option value="{{ \App\Enums\MessageDocumentTypeEnum::TYPE_PROTEST_DOC }}">
+            {{ \App\Enums\MessageDocumentTypeEnum::getMessage('TYPE_PROTEST_DOC') }}
+        </option>
         <option value="{{ \App\Enums\MessageDocumentTypeEnum::OTHER }}">
             {{ \App\Enums\MessageDocumentTypeEnum::getMessage('OTHER') }}
         </option>
